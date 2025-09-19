1468074658f711d629c8f2957d1f94d7590bcc8c
Michael Salamakha <MichaelSalamakha@gmail.com>
2025-09-19 16:36:20 +0200

    fix: remove outdated integration examples with sms and email (#60)
    Remove outdated integration examples for sms and email that are not supported.

--- examples/resources/uptimerobot_integration/multiple.tf
+++ examples/resources/uptimerobot_integration/multiple.tf
@@ -5,15 +5,6 @@ resource "uptimerobot_monitor" "website" {
   interval = 300
 }
 
-# Email integration for general alerts
-resource "uptimerobot_integration" "team_email" {
-  name                     = "Team Email"
-  type                     = "email"
-  value                    = "alerts@example.com"
-  enable_notifications_for = 1
-  ssl_expiration_reminder  = true
-}
-
 # Slack integration for team notifications
 resource "uptimerobot_integration" "team_slack" {
   name                     = "Team Slack"
@@ -22,15 +13,6 @@ resource "uptimerobot_integration" "team_slack" {
   custom_value             = "#monitoring"
   enable_notifications_for = 1
   ssl_expiration_reminder  = true
-}
-
-# SMS for critical down events only
-resource "uptimerobot_integration" "emergency_sms" {
-  name                     = "Emergency SMS"
-  type                     = "sms"
-  value                    = "+1234567890"
-  enable_notifications_for = 2 # Down events only
-  ssl_expiration_reminder  = false
 }
 
 # Webhook for external systems
