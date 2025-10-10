405546892131aae35e93df3dbb848fb4225039a4
Michael Salamakha <michaelsalamakha@gmail.com>
2025-10-11 01:51:02 +0200

    docs: ignore config example

--- examples/resources/uptimerobot_monitor/config_examples.tf
+++ examples/resources/uptimerobot_monitor/config_examples.tf
@@ -39,3 +39,20 @@ resource "uptimerobot_monitor" "clear" {
     ssl_expiration_period_days = []
   }
 }
+
+resource "uptimerobot_monitor" "ui_driven_ssl" {
+  name     = "UI-driven SSL days"
+  type     = "HTTP"
+  url      = "https://example.com"
+  interval = 300
+
+  ssl_expiration_reminder = true
+
+  # If it is managed via UI, then adding ignore_changes in lifecycle tells Terraform to not reconcile and take in account changes here.
+  lifecycle {
+    ignore_changes = [config]
+  }
+
+  # Optional. The block may be kept to preserve remote values explicitly
+  config = {}
+}
