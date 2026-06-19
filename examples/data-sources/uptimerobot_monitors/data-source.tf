ed9b2fca6b714b2f6586f19de96d6761f4fdd305
Michael Salamakha <michaelsalamakha@gmail.com>
2026-06-19 16:51:10 +0200

    docs: show monitors data source maintenance usage

--- examples/data-sources/uptimerobot_monitors/data-source.tf
+++ examples/data-sources/uptimerobot_monitors/data-source.tf
@@ -7,9 +7,26 @@ data "uptimerobot_monitors" "production_api" {
   }
 }
 
+locals {
+  production_api_monitor_ids = toset([
+    for id in data.uptimerobot_monitors.production_api.ids : tonumber(id)
+  ])
+}
+
 resource "uptimerobot_psp" "production" {
   name        = "Production Status"
-  monitor_ids = data.uptimerobot_monitors.production_api.ids
+  monitor_ids = local.production_api_monitor_ids
+}
+
+resource "uptimerobot_maintenance_window" "production_api" {
+  name     = "Production API Maintenance"
+  interval = "weekly"
+  duration = 60
+  time     = "02:00:00"
+  days     = [7]
+
+  auto_add_monitors = false
+  monitor_ids       = local.production_api_monitor_ids
 }
 
 output "production_api_monitors" {
